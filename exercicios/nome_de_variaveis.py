5c8e8ca4a488483864d408c2e70817bc88d3258e
Lúcio Gomes de Oliveira <160409440+lusin69@users.noreply.github.com>
2024-02-20 16:14:58 -0300

    nome de variaveis

---
+++ exercicios/nome_de_variaveis.py
@@ -0,0 +1,82 @@
+# Nomes de variáveis
+# Uma variável pode ter um nome curto (como x e y) ou um nome mais descritivo (idade, nome do carro, volume_total).
+# Regras para variáveis Python:
+# 1 - O nome de uma variável deve começar com uma letra ou sublinhado
+# 2 - Um nome de variável não pode começar com um numero
+# 3 - Um nome de uma variavel só pode conter caracteres alfanumericos e sublinhados (Az,  0-9 e _)
+# 4 - Os nomes das variáveis diferenciam maiúsculas de minúsculas (idade, Idade e IDADE são três variáveis diferentes)
+# 5 - Um nome de variável não pode ser nenhuma palavra-chave do python
+
+# palavras-chave
+'''
+and 
+as 
+assert 
+break
+class
+continue
+def
+del
+elif
+else
+except
+False
+finally
+for
+fron
+global
+if
+import
+in
+is
+lambda
+None
+nonlocal
+not
+or
+pass
+raise
+return
+True
+try
+while
+with
+yield
+'''
+# EXEMPLO
+# Nomes legais de variáveis:
+
+myvar = 'John'
+my_var = 'John'
+_my_var = 'John'
+myVar = 'John'
+MYVAR = 'John'
+myvar2 = 'John'
+
+# EXEMPLOS 
+# Nomes de variáveis ilegais:
+
+2myvar = 'John'
+my-var = 'John'
+my var = 'John'
+
+'''
+Observação: lembre-se de que os nomes das variáveis diferenciam maiúsculas de minúsculas
+'''
+# Nome de variáveis com múltiplas palavras
+
+# Nomes de variaveis com mais de uma palavra podem ser difíceis de ler.
+
+# Existem várias técnicas que você pode usar para torna-los mais legíveis:
+
+# Camel case (Cada palavra, exceto a primeira, começa com letra maiúscula:)
+
+myVariableName = 'John'
+
+# Pascal case (Cada palavra começa com uma letra maiúscula):
+
+MyVariableName = 'John'
+
+# snack case (cada palavra é separada por um caractere de sublinhado):
+
+my_variable_name = 'John'
