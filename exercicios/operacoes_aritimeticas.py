68499b90ad3edebe86e0a01d1d451c6cdadc72f8
Lúcio Gomes de Oliveira <160409440+lusin69@users.noreply.github.com>
2024-02-22 09:01:42 -0300

    operadorers

---
+++ exercicios/operacoes_aritimeticas.py
@@ -0,0 +1,13 @@
+# Operadores aritimeticos
+'''
++ usado para adição ex: 5 + 2 == 7
+- usado ára subtração ex: 5 - 2 == 3
+* usado para multiplicação ex: 5 * 2 == 10
+/ usado para divisão ex: 5 / 2 == 2.5
+** usado para potencia ex: 5 ** 2 == 25
+// usado para divisão inteira ex: 5 // 2 == 2
+% usado para resto da divisão ex: 5 % 2 == 1
+== usado como igualdade
+= usada como recebe
+
+'''
