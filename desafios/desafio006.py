1bcdf3733c5fe3174059efbe0f82c6a0c7ab2853
Lúcio Gomes de Oliveira <160409440+lusin69@users.noreply.github.com>
2024-02-23 10:48:23 -0300

    Create desafio006.py

---
+++ desafios/desafio006.py
@@ -0,0 +1,5 @@
+n = int(input('digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('Analizando o numero {} temos: \n Seu dobro {} \n e o seu triplo {} \n ja a raiz quadrada é {} '.format(n, d, t, r))
