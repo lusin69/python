7768fd4788230320c53d1512ffb37dd717a142c5
Lúcio Gomes de Oliveira <luciowar18@gmail.com>
2024-02-24 17:06:54 -0300

    Update desafio011.py

--- desafios/desafio011.py
+++ desafios/desafio011.py
@@ -4,4 +4,4 @@ altura = float(input('Digite a altura da parede: '))
 area = largura * altura
 print('Sua parede tem a dimenção de {}m x {}m e a sua area é {}m².'.format(largura, altura, area))
 tinta = area / 2
-print('Para pintar essa parede você precisa-ra de {}L'.format(tinta))
+print('Para pintar essa parede você precisa-ra de {:.3f}L'.format(tinta))
